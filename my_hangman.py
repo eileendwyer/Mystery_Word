c398c96d7e6f0526eef0c1ea7ca81fe32c886a61
Eileen Dwyer <eolson@smalltalku.com>
2016-05-21 08:36:37 -0400

    second draft my_hangman normal mode

--- my_hangman.py
+++ my_hangman.py
@@ -1,39 +1,46 @@
-#  /usr/share/dict/words
 #  random
-# how many letters the computer's word contains
-# user input is one guess per round case insensitive
-# feedback to usr about their letter guess - Guesses = 8 max - & how many
-# guesses remaining - lose a guess only if guess is incorrect
-# usr feedback if they duplicate guess - print(try again)
-# access letters in word by index to display the partially guessed word
-# end game = guess the word, use all guesses - if yes show the word
-#
+#  /usr/share/dict/words
 import random
 with open("/usr/share/dict/words") as opened_file:
     word_list = (opened_file.read().upper().split())
     secret_word = list(random.choice(word_list))
-
-number_guesses = 1
-
-welcome_player = input("Welcome to the secret word game. There are {} letters in the secret word. "
+    secret_word[0] = word_list[0]
+# how many letters the computer's word contains
+welcome_player = input("\nWelcome to the secret word game. There are {} letters in the secret word. "
                        .format(len(secret_word)))
-bad_guess =[]
-correct_guess = []
+def get_guess():
+    # user input is one guess per round case insensitive
+    # feedback to usr about their letter guess - Guesses = 8 max - & how many
+    secret_word = []
+    incorrect_guess = []
+    repeat_guess = []
+    max_guesses = 8
 
-def get_guess(bad_guess, correct_guess):
     player_guess = input("Guess a letter:  ").upper()
-    if len(player_guess) != 1:
-        print("You can only guess one letter per turn.")
-    elif player_guess in bad_guess or correct_guess:
+ # guesses remaining
+    if player_guess in len(secret_word):
+        print(secret_word.append('_'))
+ # show correct guesses
+        print("Good guess! You have {} guesses remaining. ".format(max_guesses))
+# lose a guess only if guess is incorrect
+    elif player_guess not in secret_word or repeat_guess:
+        incorrect_guess.append(player_guess)
+        max_guesses -=1
+ # usr feedback if they duplicate guess - print(try again)
+    elif player_guess in repeat_guess or incorrect_guess and player_guess not in secret-word:
+        repeat_guess.append(player_guess)
         print("You already guessed that letter! ")
-    elif notguess.isalpha():
+
+    elif len(player_guess) != 1:
+        print("You can only guess one letter per turn.")
+
+    elif player_guess.notisalpha():
         print("You can only guess letters!")
     else:
-        return guess
-get_guess()
+        return player_guess
 
-def track_guess(bad_guess, correct_guess, secret_word):
-    for letter in bad_guess:
+def track_guess(secret_word, incorrect_guess, correct_guess):
+    for letter in incorrect_guess:
         print('\n\n')
 
     for letter in secret_word:
@@ -42,16 +49,9 @@ def track_guess(bad_guess, correct_guess, secret_word):
         else:
             print('_' , end='')
             print('')
+# end game = guess the word, use all guesses - show the word
+def game_over():
 
-
-    for letter in word:
-        print(secret_word.append('_'))
-        secret_word[0] = word[0]
-
-        while player_guesses < 9:
-            if player_guesses in secret_word:
-                player_guesses = good_guesses
-                print("Good guess. You have {} guesses remaining. ".format(number_guesses))
-                number_guesses += 1
-
+            get_guess()
+            game_over()
 
